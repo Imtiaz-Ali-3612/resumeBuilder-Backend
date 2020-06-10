73734f2b9e01940674b0c510e4ec4fd27c9d8ffc
Imtiaz-Ali-3612 <imtiaz.ali.3612@gmail.com>
2020-06-10 14:54:06 +0500

    Added Routes for Resume

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -8,5 +8,13 @@ var router =require('express').Router(),
 router.get('/userinfo/',verifyUser.verifyToken,resumeController.getUserInfo)
 router.post('/userinfo/',verifyUser.verifyToken,resumeController.setUserInfo)
 
+router.get('/educationinfo/',verifyUser.verifyToken,resumeController.getEducationInfo)
+router.post('/educationinfo/',verifyUser.verifyToken,resumeController.setEducationInfo)
+
+router.get('/experianceinfo/',verifyUser.verifyToken,resumeController.getExperianceInfo)
+router.post('/experianceinfo/',verifyUser.verifyToken,resumeController.setExperianceInfo)
+
+router.get('/projectinfo/',verifyUser.verifyToken,resumeController.getProjectInfo)
+router.post('/projectinfo/',verifyUser.verifyToken,resumeController.setProjectInfo)
 
 module.exports=router;
