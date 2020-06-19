c868bdad883757b7e0fb4450f221631102e2d7e4
Imtiaz-Ali-3612 <imtiaz.ali.3612@gmail.com>
2020-06-19 22:03:23 +0500

    added configuration of deployed MYSQL

--- dbConfig.js
+++ dbConfig.js
@@ -2,10 +2,10 @@ var mysql=require('mysql2')
 
 
 var pool = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    database:"resumebuilder",
-    password:"fast3612"
+    host:"sql12.freemysqlhosting.net",
+    user:"sql12349576",
+    database:"sql12349576",
+    password:"syzSUAIa4a"
 });
 
 module.exports=pool.promise();
