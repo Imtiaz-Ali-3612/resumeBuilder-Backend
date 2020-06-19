4acd6bb28d746d03b295565aeb0f41595eaac1fb
Imtiaz-Ali-3612 <imtiaz.ali.3612@gmail.com>
2020-06-19 22:16:36 +0500

    port changed

--- dbConfig.js
+++ dbConfig.js
@@ -4,7 +4,7 @@ var mysql=require('mysql2')
 var pool = mysql.createPool({
     host:"sql12.freemysqlhosting.net",
     user:"sql12349576",
-    database:"sql12349576",
+    // database:"sql12349576",
     password:"syzSUAIa4a"
 });
 
