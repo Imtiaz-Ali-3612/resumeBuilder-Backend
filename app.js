511c4b7c01129a280e6b41952b6880b280d2dc6d
unknown <imtiaz@proleadsoft.com>
2021-01-23 10:49:05 -0800

    new image route

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express=require('express'),
     bodyParser=require('body-parser'),
     config=require('./config'),
-    jwt=require('jsonwebtoken');
+    jwt=require('jsonwebtoken'),
+     fs = require('fs');
+    
 
 
 var resumeRoutes=require('./routes/resumeRoutes'),
@@ -12,6 +14,43 @@ app.use(bodyParser.json())
 
 app.get('/',(req,res)=>{
     res.send('<div><h1>Welcome To the Resume Builder App Back-End</h1><br> <i>contact: imtiaz.ali.3612@gmail.com</i></div>')
+})
+function base64_encode(file) {
+    var bitmap = fs.readFileSync(file);
+    return new Buffer(bitmap).toString('base64');
+}
+app.get('/retrieve-img',(req,res)=>{
+    let files={
+        file1:{
+            name:"item1",
+            brand:"brand1",
+            path:"./images/1.jpg"
+        },
+        file2:{
+            name:"item2",
+            brand:"brand2",
+            path:"./images/2.jpeg"
+        },
+        file3:{
+            name:"item3",
+            brand:"brand3",
+            path:"./images/3.png"
+        },
+        file4:{
+            name:"item4",
+            brand:"brand4",
+            path:"./images/4.jpg"
+        },
+        file5:{
+            name:"item5",
+            brand:"brand5",
+            path:"./images/5.jpg"
+        }
+    }
+   let randomImage=files["file"+(Math.floor(Math.random() * 5+1))]
+    let bas64=base64_encode(randomImage.path)
+    randomImage.path=bas64
+    res.send(randomImage)
 })
 
 app.use('/user',userRoutes);
